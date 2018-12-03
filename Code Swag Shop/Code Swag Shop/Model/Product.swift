9cb1ad37e2793eb61c923e20cf2594e750782ce0
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-03 19:26:28 +0800

    running with data models

---
+++ Code Swag Shop/Code Swag Shop/Model/Product.swift
@@ -0,0 +1,22 @@
+//
+//  Product.swift
+//  Code Swag Shop
+//
+//  Created by Jeffrey Umandap on 03/12/2018.
+//  Copyright © 2018 Jeffrey Umandap. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private (set) public var title: String
+    private (set) public var price: String
+    private (set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+    
+}
