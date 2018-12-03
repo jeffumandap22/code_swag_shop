a83e643bbcda8609b3891b825991d2bfb232d9a4
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-03 18:43:32 +0800

    Collection Views (Grid Layouts)

---
+++ Code Swag Shop/Code Swag Shop/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  Code Swag Shop
+//
+//  Created by Jeffrey Umandap on 03/12/2018.
+//  Copyright © 2018 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
